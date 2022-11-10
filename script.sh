c600e0bab58b8893d15f78ba6bf7d697548494a5
Tilen Komel <tilen.komel10@gmail.com>
2022-11-10 13:01:51 +0100

    fixed err on line 7

--- script.sh
+++ script.sh
@@ -8,12 +8,12 @@ mkdir -p /backup/daily/ /backup/weekly/ /backup/logs/
 
 echo "---- ${date} ----" >> /backup/logs/daily.log
 
-if [[$daten -eq 7 ]]; then
+if [[ $daten -eq 7 ]]; then
     echo "---- ${date} ----" >> /backup/logs/weekly.log
 
-    rsync -rPu -e 'ssh -i /scripts/key -o "StrictHostKeyChecking no"' "${SSH_STR}:${PAT_TO_ORIG}" /backup/weekly
+    rsync -rPu -e 'ssh -i /scripts/key -o "StrictHostKeyChecking no"' "${SSH_STR}:${PATH_TO_ORIG}" /backup/weekly
     echo "Rsync exit code: ${?}" >> /backup/logs/weekly.log
 fi
 
-rsync -rPu -e 'ssh -i /scripts/key -o "StrictHostKeyChecking no"' "${SSH_STR}:${PAT_TO_ORIG}" /backup/daily
+rsync -rPu -e 'ssh -i /scripts/key -o "StrictHostKeyChecking no"' "${SSH_STR}:${PATH_TO_ORIG}" /backup/daily
 echo "Rsync exit code: ${?}" >> /backup/logs/daily.log
